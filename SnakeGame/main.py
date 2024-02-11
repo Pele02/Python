df98628d4fb32e7eb29e7d11cdc5c6adfe132c4b
Pele02 <pelealex02@gmail.com>
2024-02-11 18:04:05 +0200

    Update Scoreboard
    Added Score feature and collision with the wall, that trigger the end of the game

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food  import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -27,6 +29,12 @@ while game_on:
     #Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
+
+    #Detect collision with wall
+    if snake.head.xcor() > 299 or snake.head.xcor() < -299 or snake.head.ycor() > 299 or snake.head.ycor() < -299:
+        game_on = False
+        scoreboard.game_over()
 
 screen.exitonclick()
 
