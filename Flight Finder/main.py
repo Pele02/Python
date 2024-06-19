5354f734a574c53b48e97352726fcbdc72748b5e
Pele02 <pelealex02@gmail.com>
2024-06-19 18:13:36 +0300

    Get Price
    The project will get you prices for flights from a given city

--- Flight Finder/main.py
+++ Flight Finder/main.py
@@ -1,16 +1,35 @@
+import datetime
+import time
 from data_manager import DataManager
 from flight_search import FlightSearch
+from datetime import datetime, timedelta
+from flight_data import find_cheapest_flight
 from pprint import pprint
 
 sheet_data = DataManager()
 flight = FlightSearch()
 
-for flight_info in sheet_data.get_sheet()["prices"]:
-    #Verify if the flight has an iata code
-    if len(flight_info["iataCode"]) == 0:
-        flight.get_IATA(flight_info)
-        id_info = flight_info["id"]
-        updated_data = {"price": flight_info}
-        sheet_data.put_sheet(id_info, updated_data)
+# Set your origin airport
+ORIGIN_CITY_IATA = "LON"
 
-#pprint(sheet_data.get_sheet())
+for flight_info in sheet_data.get_sheet()["prices"]:
+    flight_info["iataCode"] = flight.get_destination_code(flight_info["city"])
+    id_info = flight_info["id"]
+    updated_data = {"price": flight_info}
+    sheet_data.put_sheet(id_info, updated_data)
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data.get_sheet()["prices"]:
+    print(f"Getting flights for {destination['city']}...")
+    flights = flight.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+    cheapest_flight = find_cheapest_flight(flights)
+    print(f"{destination['city']}: £{cheapest_flight.price}")
+    # Slowing down requests to avoid rate limit
+    time.sleep(2)
