81195d3496ab4c7a627a06e7dd4bbf37d1e2a6e9
Pele02 <pelealex02@gmail.com>
2024-07-29 19:39:34 +0300

    SQL database update

--- My Library/main.py
+++ My Library/main.py
@@ -1,56 +1,57 @@
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
-'''
-On Windows type:
-python -m pip install -r requirements.txt
 
-On MacOS type:
-pip3 install -r requirements.txt
+class Base(DeclarativeBase):
+    pass
 
-This will install the packages from requirements.txt for this project.
-'''
+
+db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
-
-all_books = []
-db = sqlite3.connect("books-collection.db")
-cursor = db.cursor()
-
-# cursor.execute("CREATE TABLE books "
-#                "(id INTEGER PRIMARY KEY, "
-#                "title varchar(250) NOT NULL UNIQUE, "
-#                "author varchar(250) NOT NULL, "
-#                "rating FLOAT NOT NULL)")
-
-cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-db.commit()
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books-collection.db"
+# initialize the app with the extension
+db.init_app(app)
 
 
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    book: Mapped[str] = mapped_column(String(250), nullable=False, unique=True)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
 
-def to_dict(book_name, book_author, book_rating):
-    book_dict = {
-        "title": book_name,
-        "author": book_author,
-        "rating": book_rating
-    }
-    return book_dict
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
 def home():
-    return render_template("index.html", all_books=all_books)
+    result = db.session.execute(db.select(Book).order_by(Book.book))
+    all_books = result.scalars()
+    return render_template("index.html", all_books = all_books)
 
 
 @app.route("/add", methods=["POST", "GET"])
 def add():
     if request.method == "POST":
-        book = request.form["book"]
-        author = request.form["author"]
-        rating = request.form["rating"]
-        all_books.append(to_dict(book, author, rating))
+        new_book = Book(book=request.form["book"], author=request.form["author"], rating=request.form["rating"])
+        db.session.add(new_book)
+        db.session.commit()
+
+        return redirect(url_for("home"))
 
     return render_template("add.html")
+
+@app.route("/edit/<int:id>")
+def edit(id):
+    book_to_update = db.session.execute(db.select(Book).where(Book.id == id)).scalar()
+    book_to_update.rating =
+    db.session.commit()
 
 
 if __name__ == "__main__":
