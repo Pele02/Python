96ef7d1e9a7b91b0b00ee423f74d3c1aad0f0fc3
Pele Mihnea-Alexandru (uif69455) <mihnea-alexandru.pele@continental-corporation.com>
2024-08-02 13:37:34 +0300

    feature/add-movie

--- Movie List/main.py
+++ Movie List/main.py
@@ -1,33 +1,36 @@
-import validators
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
-from flask_wtf import Form, FlaskForm
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 import requests
 
+# API key
+API_KEY = "30b367d1ee0d29802092ddfebcbf401d"
+url = "https://api.themoviedb.org/3/search/movie"
 
-#Create the database object
+
+# Create the database object
 class Base(DeclarativeBase):
     pass
 
 
 db = SQLAlchemy(model_class=Base)
 
-#Create the app
+# Create the app
 app = Flask(__name__)
-#Configure the SQLite database, relative to the app instance folder
+# Configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-#Initialize the app with the extension
+# Initialize the app with the extension
 db.init_app(app)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
 
-#Define the model for database
+# Define the model for database
 class Movie(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
@@ -38,13 +41,20 @@ class Movie(db.Model):
     review: Mapped[str] = mapped_column(String(500), nullable=False)
     img_url: Mapped[str] = mapped_column(String(1000), unique=True, nullable=False)
 
-#Define the model for Form
+
+# Define the model for Form
 class RatingForm(FlaskForm):
-    rating = StringField(label='Your Rating out of 10 e.g. 7.5', validators=[DataRequired(), ])
+    rating = StringField(label='Your Rating out of 10 e.g. 7.5', validators=[DataRequired()])
     review = StringField(label='Your Review', validators=[Length(min=1, max=75)])
     submit = SubmitField(label='Done')
 
-#CREATE TABLE
+
+class AddMovieForm(FlaskForm):
+    movie_name = StringField(label='Enter the movie name', validators=[DataRequired()])
+    submit = SubmitField(label='Add Movie')
+
+
+# CREATE TABLE
 with app.app_context():
     db.create_all()
 
@@ -54,10 +64,11 @@ def home():
     movies = db.session.execute(db.select(Movie)).scalars()
     return render_template("index.html", movies=movies)
 
-@app.route("/edit movie", methods = ["POST", "GET"])
+
+@app.route("/edit movie", methods=["POST", "GET"])
 def edit_movie():
     movie = db.get_or_404(Movie, request.args.get("id"))
-    form = RatingForm(obj=movie)
+    form = RatingForm()
 
     if form.validate_on_submit():
         movie.rating = float(form.rating.data)
@@ -65,6 +76,27 @@ def edit_movie():
         db.session.commit()
         return redirect(url_for('home'))
     return render_template("edit.html", movie=movie, form=form)
+
+
+@app.route("/delete")
+def delete_movie():
+    movie = db.get_or_404(Movie, request.args.get("id"))
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+@app.route("/add", methods=["POST", "GET"])
+def add_movie():
+    form = AddMovieForm()
+    if form.validate_on_submit():
+        movie_title = form.movie_name.data
+        response = requests.get(url=url, params={"api_key": API_KEY, "query": movie_title})
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+
+    return render_template("add.html", form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
